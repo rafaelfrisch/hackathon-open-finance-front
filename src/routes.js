942f93d4cfc964fecae737d540fed5a0175ea905
Luiz <lvj253@gmail.com>
2022-09-24 15:09:18 -0300

    page Register

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import HomePage from './pages/Homepage/index'
+import Register from './pages/Register';
 
 const RoutesPages = () => (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/register" element={<Register/>}/>
         </Routes>
     </BrowserRouter>       
 );
